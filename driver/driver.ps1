09c0256012f98737bb413533a04b5a948dcfee0e
Mark Garner <markgarner@Tempe-TestX333.redmond.corp.microsoft.com>
2020-10-22 19:34:46 -0500

    generalized base url

--- driver/driver.ps1
+++ driver/driver.ps1
@@ -1,37 +1,38 @@
-
+$baseUrl = "https://micro-apim.azure-api.net"
+$thinkTime = 300
 $stopTime = (Get-Date).AddSeconds(300)
 
 $stopTime
 while ($stopTime -gt (Get-Date)) {
     Write-Host "Requesting..."
     
-        $url = "https://micro-cart.azurewebsites.net/cart"
+        $url = $baseUrl + "/cart/cart"
         $body = @{
             Name='blah'
         }
         $json = $body | ConvertTo-Json
         $cart = Invoke-RestMethod -Method 'Post' -Uri $url -Body $json -ContentType 'application/json'
 
-        Start-Sleep -Milliseconds 600
+        Start-Sleep -Milliseconds $thinkTime
 
-        $url = "https://micro-catalog.azurewebsites.net/catalog"
+        $url = $baseUrl + "/catalog/catalog"
         $catalogItems = Invoke-RestMethod -Method 'Get' -Uri $url
 
         $numberOfItemsToAdd = Get-Random -Minimum 1 -Maximum 5
 
-        Start-Sleep -Milliseconds 600
+        Start-Sleep -Milliseconds $thinkTime
 
         for($i = 0; $i -lt $numberOfItemsToAdd; $i++)
         {
-            $url = "https://micro-cart.azurewebsites.net/cart/" + $cart.id + "/AddItemToCart/" + $catalogItems[$i].Id
+            $url = $baseUrl + "/cart/cart/" + $cart.id + "/AddItemToCart/" + $catalogItems[$i].Id
             Invoke-RestMethod -Method 'Post' -Uri $url
-            Start-Sleep -Milliseconds 600
+            Start-Sleep -Milliseconds $thinkTime
         }
 
-        $url = "https://micro-cart.azurewebsites.net/cart/" + $cart.id + "/Checkout"
+        $url = $baseUrl + "/cart/cart/" + $cart.id + "/Checkout"
         Invoke-RestMethod -Uri $url -Method 'Post' -ContentType 'application/json'
     
-        Start-Sleep -Milliseconds 600
+        Start-Sleep -Milliseconds $thinkTime
 }
 
 
