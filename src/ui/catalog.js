75e2d7c70394a85d669ed5cbc8b71da6643b277c
Mark Garner <mgarner@microsoft.com>
2020-12-23 17:23:27 -0600

    first ui screen

--- src/ui/catalog.js
+++ src/ui/catalog.js
@@ -1,18 +1,30 @@
-var Animal = Backbone.Model.extend({
-    defaults: {
-      name: 'Fido',
-      color: 'black',
-      sound: 'woof'
-    },
-    validate: function(attrs, options){
-      if (!attrs.name){
-          alert('Your animal must have a name!');
-      }
-      if (attrs.name.length < 2){
-          alert('Your animal\'s name must have more than one letter!');
-      }
-    },
-    sleep: function(){
-      alert(this.get('name') + ' is sleeping.');
+  
+function AppViewModel() {
+
+    var self = this;
+    self.catalogItems = ko.observableArray([]);
+
+    this.getCatalogItems = function () {
+        $.getJSON("https://micro-catalog-web-2tg.azurewebsites.net/catalog", function(data) { 
+    
+            var items = data.map(ConvertToCatalogItem);
+            self.catalogItems(items)
+        })
+    };
+
+    function ConvertToCatalogItem(item) {
+
+        var catalogItem = {};
+        catalogItem.id = item.id;
+        catalogItem.name = item.name;
+        catalogItem.description = item.description;
+        catalogItem.price = item.price;
+
+        return catalogItem;
     }
-  });
+}
+
+// Activates knockout.js
+var vm = new AppViewModel();
+ko.applyBindings(vm);
+vm.getCatalogItems();
